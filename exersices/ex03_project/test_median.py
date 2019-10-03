3c1c0d4b17bbcae5aaf526ceb78c3876c0e1c8d7
Ashild <ashild.grotan@nmbu.no>
2019-10-03 14:49:21 +0200

    new test: test_median_of_ordered_and_unordered_elements():

--- exersices/ex03_project/test_median.py
+++ exersices/ex03_project/test_median.py
@@ -37,3 +37,9 @@ def test_median_of_odd_number_of_elements():
 
 def test_median_of_even_number_of_elements():
     assert median([1, 2, 4, 5]) == 3
+
+
+def test_median_of_ordered_and_unordered_elements():
+    assert median([1, 2, 3, 4, 5]) == 3
+    assert median([5, 2, 4, 1, 3]) == 3
+    assert median([5, 4, 3, 2, 1]) == 3
