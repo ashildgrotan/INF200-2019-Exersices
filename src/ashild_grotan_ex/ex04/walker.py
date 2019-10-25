bb42a05b25fedecb0726c82abb2b37e211e3e976
Ashild <ashild.grotan@nmbu.no>
2019-10-25 09:51:46 +0200

    Finished walker with main program

--- src/ashild_grotan_ex/ex04/walker.py
+++ src/ashild_grotan_ex/ex04/walker.py
@@ -11,6 +11,7 @@ class Walker:
     def __init__(self, x0, h):
         self.position = x0
         self.home = h
+        self.steps = 0
 
     def move(self):
         """Move one step in a random direction (back or forth)"""
@@ -19,6 +20,7 @@ class Walker:
             self.position += 1
         if dice <= 3:
             self.position -= 1
+        self.steps += 1
         return self.position
 
     def is_at_home(self):
@@ -29,16 +31,22 @@ class Walker:
 
     def get_position(self):
         """Check where the student is"""
-        pass
+        return self.position
 
     def get_steps(self):
         """Check how many steps the student has taken. """
-        pass
+        return self.steps
+
+
+def one_walk_home(start, home):
+    """Uses the class Walker to simulate a whole walk home"""
+    student = Walker(start, home)
+    while student.is_at_home() is False:
+        student.move()
+    return student.steps
 
 
 if __name__ == "__main__":
-    student = Walker(0, 3)
-    for _ in range(20):
-        student.move()
-        print(student.position)
-        print(student.is_at_home())
+    for distance in [1, 2, 5, 10, 20, 50, 100]:
+        print(f'Distance: {distance} -> Path lengths: '
+              f'{[one_walk_home(0, distance) for _ in range(5)]}')
